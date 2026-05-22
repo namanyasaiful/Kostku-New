08bd0190958e2b81cac94791ed3194581013ba2d
namanyasaiful <saifullohfattah023@gmail.com>
2026-05-22 09:40:03 +0700

    fix: role protected view

--- app/Http/Controllers/Pengelola/PenghuniPengelolaController.php
+++ app/Http/Controllers/Pengelola/PenghuniPengelolaController.php
@@ -3,10 +3,27 @@
 namespace App\Http\Controllers\Pengelola;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class PenghuniPengelolaController extends Controller
 {
+        public function guardPengelola()
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        if (Auth::user()->role !== 'pengelola') {
+            if (Auth::user()->role === 'penghuni') {
+                return redirect()->route('dashboard.penghuni');
+            }
+
+            return redirect()->route('dashboard.superadmin');
+        }
+
+        return null;
+    }
     public function viewPenghuni()
     {
         return view('pages.pengelola.penghuni-pengelola');
