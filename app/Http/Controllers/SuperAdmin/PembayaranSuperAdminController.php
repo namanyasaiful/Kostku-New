cb6eab90408935d23b54954b2edf1c6ab62ac3b2
vinanrs25 <vinanurnisa225@gmail.com>
2026-06-06 19:09:13 +0700

    add: backend pembayaran superadmin and fix pengaduan superadmin

--- app/Http/Controllers/SuperAdmin/PembayaranSuperAdminController.php
+++ app/Http/Controllers/SuperAdmin/PembayaranSuperAdminController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers\SuperAdmin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Pembayaran;
 use Illuminate\Http\Request;
 
 class PembayaranSuperAdminController extends Controller
 {
-    public function viewPembayaranSuperAdmin()
+    public function viewPembayaranSuperAdmin(Request $request)
     {
-        return view('pages.superadmin.pembayaran-superadmin');
+        $search = $request->input('search');
+
+        $pembayarans = Pembayaran::with(['user.penghuni.kamar.kost'])
+            ->when($search, function ($query, $search) {
+                $query->where('id_pembayaran', 'like', "%{$search}%")
+                    ->orWhereHas('user', function ($q) use ($search) {
+                        $q->where('nama', 'like', "%{$search}%")
+                          ->orWhere('email', 'like', "%{$search}%");
+                    });
+            })
+            ->orderBy('created_at', 'desc')
+            ->paginate(10)
+            ->withQueryString();
+
+        return view('pages.superadmin.pembayaran-superadmin', compact('pembayarans', 'search'));
     }
 }
